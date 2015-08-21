e9974534245e06da13bf22477683c27c87dc553b
Josh <Tattsy@hotmail.co.uk>
2015-08-21 06:05:48 +0100

    now saves state of temp and city in cookies so they persist

--- app/public/appJquery.js
+++ app/public/appJquery.js
@@ -1,46 +1,76 @@
-$( document ).ready(function() {
-thermostat = new Thermostat();
+var cookieCity
+var cookieTemp
+var cookieEsm
+$.get('/cookies/city', function(city) {
 
-  var setTempColor = function(){
-    $('#temperature').html(thermostat.temperature + "\xBAC");
-    $('#temperature').css("color", thermostat.tempColor);
-  }
+  city ? cookieCity = city : cookieCity = "Dewsbury";
 
-  setTempColor();
+  $.get('/cookies/temp', function(temp) {
 
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=Dewsbury&units=metric&APPID=f46b9c5aef58799f2f61b6c7f003f5e8', function(data){
-    $('h2').text("The temperature in " + data.name + " is " + Math.round(data.main.temp) + "\xBAC");
-  });
+    temp ? cookieTemp = temp : cookieTemp = 20;
+    
+    $.get('/cookies/esm',function(esm) {
 
-  $('#increase').click(function(){
-    thermostat.raise();
-    setTempColor();
-  });
+      var thermostat = new Thermostat();
+      thermostat.temperature = cookieTemp;
 
-  $('#decrease').click(function(){
-    thermostat.lower();
-    setTempColor();
-  });
+      if (esm == "false") {
+        $('#powerSave').prop('checked', false);
+        thermostat.powerSavemode = false
+      };  
 
-  $('#reset').click(function(){
-    thermostat.resetTemp();
-    setTempColor();
-  });
+      $( document ).ready(function() {
 
-  $('#powerSave').click(function(){
-    ($("#powerSave").is(":checked")) ? thermostat.powerSaveOn() : thermostat.powerSaveOff();
-    setTempColor();
-  });
+        var setTempColor = function(){
+          $('#temperature').html(thermostat.temperature + "\xBAC");
+          thermostat.changeColor();
+          $('#temperature').css("color", thermostat.tempColor);
+        };
 
-  $('#submitLocation').click(function(){
-    var city = $('#cityName').val()
-    if (city) {
-      $.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&APPID=f46b9c5aef58799f2f61b6c7f003f5e8', function(data){
-        $('h2').text("The temperature in " + data.name + " is " + Math.round(data.main.temp) + "\xBAC");
-        $('h3').text("")
+        setTempColor()
+
+        $.get('http://api.openweathermap.org/data/2.5/weather?q='+cookieCity+'&units=metric&APPID=f46b9c5aef58799f2f61b6c7f003f5e8', function(data){
+          $('h2').text("The temperature in " + data.name + " is " + Math.round(data.main.temp) + "\xBAC");
+        });
+
+        $('#increase').click(function(){
+          thermostat.raise();
+          setTempColor();
+          $.get('setcookie/temp/'+thermostat.temperature);
+        });
+
+        $('#decrease').click(function(){
+          thermostat.lower();
+          setTempColor();
+          $.get('setcookie/temp/'+thermostat.temperature);  
+        });
+
+        $('#reset').click(function(){
+          thermostat.resetTemp();
+          setTempColor();
+          $.get('setcookie/temp/'+thermostat.temperature);
+        });
+
+        $('#powerSave').click(function(){
+          ($("#powerSave").is(":checked")) ? thermostat.powerSaveOn() : thermostat.powerSaveOff();
+          setTempColor();
+          $.get('setcookie/temp/'+thermostat.temperature);
+          $.get('setcookie/esm/'+thermostat.powerSavemode);
+        });
+
+        $('#submitLocation').click(function(){
+          var city = $('#cityName').val()
+          if (city) {
+            $.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&APPID=f46b9c5aef58799f2f61b6c7f003f5e8', function(data){
+              $('h2').text("The temperature in " + data.name + " is " + Math.round(data.main.temp) + "\xBAC");
+              $('h3').text("")
+            });
+            $.get('setcookie/city/'+city);
+          } else {
+            $('h3').text("You have not entered a city")
+          };
+        });
       });
-    } else {
-      $('h3').text("You have not entered a city")
-    };
+    });
   });
 });
--- app/server.rb
+++ app/server.rb
@@ -2,9 +2,38 @@ require "sinatra/base"
 require "sinatra/cookies"
 
 class Thermostat < Sinatra::Base
-  
-  get "/" do
+
+  helpers Sinatra::Cookies
+
+  get '/' do
     erb :index
+  end
+
+  get '/setcookie/city/:city' do
+    cookies[:city] = params[:city]
+    erb :city 
+  end
+
+  get '/cookies/city' do
+    erb :city
+  end
+
+  get '/setcookie/temp/:temp' do
+    cookies[:temp] = params[:temp]
+    erb :temp
+  end
+
+  get '/cookies/temp' do
+    erb :temp
+  end
+
+  get '/setcookie/esm/:esm' do
+    cookies[:esm] = params[:esm]
+    erb :esm
+  end
+
+  get '/cookies/esm' do
+    erb :esm
   end
 
   # start the server if ruby file executed directly
